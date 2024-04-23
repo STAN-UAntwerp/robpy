205d3435d6ea49ed373e9a085f3df21bac71a56a
Thomas Servotte <servottethomas@gmail.com>
2024-04-23 15:48:09 +0200

    refactor: reusable code + tests + fix: flake8 and black

--- robpy/datasets/_base.py
+++ robpy/datasets/_base.py
@@ -1,9 +1,26 @@
 import pathlib
 import pandas as pd
+import numpy as np
 from sklearn.utils import Bunch
 
 DATA_FOLDER = pathlib.Path(__file__).parent / "data"
 DESCR_FOLDER = pathlib.Path(__file__).parent / "descr"
+
+
+def _load_data_and_descr(
+    data_file_name: str, descr_file_name: str, as_frame: bool, feature_names: list[str]
+) -> tuple[pd.DataFrame | np.ndarray, str]:
+    df = pd.read_csv(DATA_FOLDER / data_file_name)
+
+    with open(DESCR_FOLDER / descr_file_name, "r") as f:
+        fdescr = f.read()
+
+    data = df[feature_names]
+
+    if not as_frame:
+        data = data.values
+
+    return data, fdescr
 
 
 def load_telephone(*, as_frame=False):
@@ -58,16 +75,12 @@ def load_telephone(*, as_frame=False):
     >>> mm = MMEstimator().fit(data.data[:, 0], data.data[:, 1])
     """
     data_file_name = "telephone.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-
-    with open(DESCR_FOLDER / "telephone.rst", "r") as f:
-        fdescr = f.read()
+    descr_file_name = "telephone.rst"
 
     feature_names = ["Year", "Calls"]
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
+    data, fdescr = _load_data_and_descr(
+        data_file_name, descr_file_name, as_frame=as_frame, feature_names=feature_names
+    )
 
     return Bunch(
         data=data,
@@ -79,9 +92,11 @@ def load_telephone(*, as_frame=False):
 
 
 def load_stars(*, as_frame=False):
-    """Load and return the Hertzsprung-Russell Diagram Data of Star Cluster CYG OB1 (covariance/regression).
+    """Load and return the Hertzsprung-Russell Diagram Data of Star Cluster CYG OB1
+    (covariance/regression).
 
-    The stars dataset is well-known bivariate dataset used for demonstrating robust covariance and regression estimators.
+    The stars dataset is well-known bivariate dataset used for demonstrating
+    robust covariance and regression estimators.
 
     =================   ==============
     Samples                         47
@@ -123,16 +138,11 @@ def load_stars(*, as_frame=False):
     >>> mm = MMEstimator().fit(data.data)
     """
     data_file_name = "stars.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-    with open(DESCR_FOLDER / "stars.rst", "r") as f:
-        fdescr = f.read()
+    descr_file_name = "stars.rst"
 
     feature_names = ["Te", "light"]
 
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
+    data, fdescr = _load_data_and_descr(data_file_name, descr_file_name, as_frame, feature_names)
 
     return Bunch(
         data=data,
@@ -188,81 +198,11 @@ def load_animals(*, as_frame=False):
     >>> mm = MMEstimator().fit(data.data)
     """
     data_file_name = "animals.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-    with open(DESCR_FOLDER / "animals.rst", "r") as f:
-        fdescr = f.read()
+    descr_file_name = "animals.rst"
 
     feature_names = ["body", "brain"]
 
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
-
-    return Bunch(
-        data=data,
-        DESCR=fdescr,
-        feature_names=feature_names,
-        filename=data_file_name,
-        data_folder=DATA_FOLDER,
-    )
-
-
-def load_animals(*, as_frame=False):
-    """Load and return the Animals2 dataset from robustbase (R) (covariance / regression).
-
-    The animals dataset is a bivariate dataset used for demonstrating robust covariance estimators.
-
-    =================   ==============
-    Samples                         65
-    Dimensionality                   2
-    Features            real, positive
-    =================   ==============
-
-    Parameters
-    ----------
-
-    as_frame : bool, default=False
-        If True, the data is a pandas DataFrame including columns with
-        appropriate dtypes (numeric).
-
-
-    Returns
-    -------
-    data : :class:`~sklearn.utils.Bunch`
-        Dictionary-like object, with the following attributes.
-
-        data : {ndarray, dataframe} of shape (65, 2)
-            The data matrix. If `as_frame=True`, `data` will be a pandas
-            DataFrame.
-        feature_names: list
-            The names of the dataset columns.
-        DESCR: str
-            The full description of the dataset.
-        filename: str
-            The path to the location of the data.
-
-
-    Examples
-    --------
-    Fitting a robust covariance estimator
-
-    >>> from robpy.datasets import load_animals
-    >>> from robpy.covariance import FastMCDEstimator
-    >>> data = load_animals()
-    >>> mm = MMEstimator().fit(data.data)
-    """
-    data_file_name = "animals.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-    with open(DESCR_FOLDER / "animals.rst", "r") as f:
-        fdescr = f.read()
-
-    feature_names = ["body", "brain"]
-
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
+    data, fdescr = _load_data_and_descr(data_file_name, descr_file_name, as_frame, feature_names)
 
     return Bunch(
         data=data,
@@ -276,7 +216,8 @@ def load_animals(*, as_frame=False):
 def load_topgear(*, as_frame=False):
     """Load and return the TopGear dataset from robustHD (R) (regression).
 
-    The TopGear dataset is a mixed variable dataset used for demonstrating robust regression estimators.
+    The TopGear dataset is a mixed variable dataset used for demonstrating
+    robust regression estimators.
 
     =================   ==============
     Samples                        297
@@ -317,12 +258,12 @@ def load_topgear(*, as_frame=False):
     >>> from robpy.datasets import load_animals
     >>> from robpy.regression import FastLTSRegressor
     >>> data = load_topgear(as_frame=True)
-    >>> mm = FastLTSRegressor().fit(data.data[["Cylinders", "Torque", "TopSpeed"]], data.data["Price"])
+    >>> mm = FastLTSRegressor().fit(
+            data.data[["Cylinders", "Torque", "TopSpeed"]], data.data["Price"]
+        )
     """
     data_file_name = "topgear.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-    with open(DESCR_FOLDER / "topgear.rst", "r") as f:
-        fdescr = f.read()
+    desc_file_name = "topgear.rst"
 
     feature_names = [
         "Maker",
@@ -380,10 +321,7 @@ def load_topgear(*, as_frame=False):
         "Origin",
     ]
 
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
+    data, fdescr = _load_data_and_descr(data_file_name, desc_file_name, as_frame, feature_names)
 
     return Bunch(
         data=data,
@@ -430,16 +368,11 @@ def load_glass(*, as_frame=False):
             The path to the location of the data.
     """
     data_file_name = "glass.csv"
-    df = pd.read_csv(DATA_FOLDER / data_file_name)
-    with open(DESCR_FOLDER / "glass.rst", "r") as f:
-        fdescr = f.read()
+    descr_file_name = "glass.rst"
 
     feature_names = [f"V{i}" for i in range(1, 751)]
 
-    data = df[feature_names]
-
-    if not as_frame:
-        data = data.to_numpy()
+    data, fdescr = _load_data_and_descr(data_file_name, descr_file_name, as_frame, feature_names)
 
     return Bunch(
         data=data,
